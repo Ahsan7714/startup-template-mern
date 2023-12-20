00f1012beb364665e7db56e0134ed78ac0d52d77
AQIB-NAWAB <aqibnawab1100@gmail.com>
2023-12-20 12:34:00 +0500

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,137 @@
+import { Link, NavLink } from "react-router-dom/dist/umd/react-router-dom.development"
+import './Navbar.css'
+import { CiSearch } from "react-icons/ci";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+const Navbar = () => {
+    const [showSidebar,setShowSidebar]=useState(false)
+    const handleSidebar=()=>{
+        setShowSidebar(!showSidebar)
+    }
+  return (
+    <>
+
+    <nav className="desktop_nav">
+        <div className="nav_container">
+            <div className="logo">
+            <Link to="/">
+                <img src="./images/logo.png" alt="" />
+
+            </Link>
+            </div>
+            <div className="nav_links">
+                <ul>
+                <li>
+
+                    <NavLink onClick={()=>handleSidebar()} exact to="/" >
+                    Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/menu">
+                    Menu
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/locations">
+                    Locations
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/mobile/app">
+                    Mobile App
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/franchise">
+                    Franchise
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/contact">
+                    Contact
+                    </NavLink>
+                </li>
+                </ul>
+            </div>
+            <div className="custom_search_bar">
+<CiSearch className="search_icon" />
+                <input type="text"  placeholder="Search" className="search_input"/>
+            </div>
+        </div>
+    </nav>
+    <nav className="mobile_nav ">
+    <div className="mobile_nav_container flex justify-between  items-center w-[96%] py-5 px-4 h-20">
+
+<div className="logo">
+    <Link to="/"><img src="./images/logo.png" alt="" /></Link>
+</div>
+    <FaBars className="text-xl cursor-pointer" onClick={()=>handleSidebar()}/>
+    </div>
+   {
+    showSidebar &&
+    (
+        <div className="mobile_nav_sidebar">
+    <div className="header flex justify-between py-5 px-4 h-20 ">
+    <div className="logo">
+    <Link to="/"><img src="./images/whiteLogo.png" alt="" /></Link>
+    </div>
+    <div className="close">
+    <FaBars className="text-xl cursor-pointer " onClick={()=>handleSidebar()}/>
+    </div>
+    </div>
+    <div className="menu">
+
+    <div className="custom_search_bar">
+<CiSearch className="search_icon" />
+                <input type="text"  placeholder="Search" className="search_input"/>
+            </div>
+    <div className="nav_links">
+                <ul>
+                <li>
+
+                    <NavLink onClick={()=>handleSidebar()} exact to="/" >
+                    Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/menu">
+                    Menu
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/locations">
+                    Locations
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/mobile/app">
+                    Mobile App
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/franchise">
+                    Franchise
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink onClick={()=>handleSidebar()} to="/contact">
+                    Contact
+                    </NavLink>
+                </li>
+                </ul>   
+                </div>
+
+            </div>
+
+    </div>
+
+    )
+   }
+    </nav>
+    </>
+
+  )
+}
+
+export default Navbar
