40c1541f860e5353831ed6a3be3df31a3cf03107
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-01-06 23:07:31 +0500

    done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -168,10 +168,8 @@ exports.login=catchAsyncError(async(req,res,next)=>{
 exports.logout=catchAsyncError(async(req,res,next)=>{
     res.cookie("token","",{
         expires: new Date(0),
-        path:"/",
         domain: "r-b.vercel.app",
-        httpOnly:true,
-        secure:true,
+        path:"/",
         sameSite:"none"
     })
     res.status(200).json({success:true,message:"Logged out successfully"})
