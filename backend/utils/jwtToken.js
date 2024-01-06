d00914e37e0d49ad27296b35be8eca5a40250a8c
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-01-06 13:13:24 +0500

    wow

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,20 @@
 const sendToken = (user, statusCode, res) => {
-    const token = user.getJWTToken();
-  
-    const options = {
-      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-    };
-  
-    res.status(statusCode).cookie("token", token, options).json({
-      success: true,
-      token,
-      user
-    });
+  const token = user.getJWTToken();
+
+  const options = {
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true, // Added for Vercel's HTTPS enforcement
+    sameSite: "Lax", // Set appropriate SameSite attribute
+    domain: "https://r-b.vercel.app", // Replace with your actual domain
+    path: "/", 
   };
-  
-  module.exports = sendToken;
-  
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+
+module.exports = sendToken;
